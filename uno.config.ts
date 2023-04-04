be0e7aca09a714a66f8d13c5ac6494f95fa67285
luohao <1223048059@qq.com>
2023-04-04 22:06:15 +0800

    clean

--- uno.config.ts
+++ uno.config.ts
@@ -4,53 +4,6 @@ import presetUno from '@unocss/preset-uno'
 import presetAttributify from '@unocss/preset-attributify'
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
-export const unoConfig: UserConfig<{}> = {
-  presets: [
-    presetIcons({
-      extraProperties: {
-        display: 'inline-block',
-        'vertical-align': 'middle',
-        margin: '5px',
-      },
-    }),
-    presetMini({
-      dark: 'class',
-    }),
-    presetUno(),
-    presetAttributify(),
-  ],
-  transformers: [transformerAttributifyJsx()],
-  rules: [
-    [
-      /**
-       * match: [ 'w-half', 'w', index: 0, input: 'w-half', groups: undefined ]
-       */
-      /^([wh])-half$/,
-      (match) => {
-        switch (match[1]) {
-          case 'w': {
-            console.log('match', match)
-            return {
-              width: '50%',
-            }
-          }
-          case 'h': {
-            return {
-              height: '50%',
-            }
-          }
-
-          default:
-            break
-        }
-      },
-    ],
-  ],
-  shortcuts: {
-    'screen-full': 'absolute w-full h-full bg-black bg-op-30 top-0 left-0 z-99',
-  },
-}
-
 export default defineConfig({
   presets: [
     presetIcons({
