944b3a8c10f350717ac91aed18ffc53089d6964f
luohao <1223048059@qq.com>
2023-04-07 21:43:47 +0800

    feat:  detach code into components

---
+++ components/IconBox.tsx
@@ -0,0 +1,32 @@
+interface IconBoxProps {
+  icon: CM.Icon
+  clickable?: boolean
+  iconShape?: 'rounded' | 'square'
+  iconWidth?: number
+  iconHeight?: number
+  iconSize?: 'xl' | '2xl' | '3xl'
+}
+
+const IconBox: React.FC<IconBoxProps> = (props) => {
+  const {
+    icon,
+    clickable = true,
+    iconShape = 'square',
+    iconWidth = 10,
+    iconHeight = 10,
+    iconSize = '2xl',
+  } = props
+  return (
+    <div
+      className={`grid pic ${
+        iconShape === 'square' ? 'rd' : 'rd-36'
+      } w-${String(iconWidth)} h-${String(iconHeight)} text-${iconSize} ${
+        clickable ? 'cursor-pointer hover:bg-light hover:bg-op-20' : ''
+      }`}
+    >
+      <i className={`${icon.content}`}></i>
+    </div>
+  )
+}
+
+export default IconBox
---
+++ components/LinkBox.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+
+interface LinkBoxProps {
+  children: ReactNode
+  active?: {
+    value: boolean
+    type: 'activeleft' | 'activebottom'
+  }
+}
+
+const LinkBox: React.FC<LinkBoxProps> = (props) => {
+  const { children, active } = props
+  return (
+    <div
+      className={`relative rd cursor-pointer ${
+        active?.value ? active.type : ''
+      }`}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default LinkBox
